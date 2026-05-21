412825631570df81c35a3b66b5763142d8aecb5c
MdMostakim103 <12mdmostakim12@gmail.com>
2026-05-21 15:50:41 +0600

    data types and variable, typecasting,comments,operators

--- firstPy.py
+++ firstPy.py
@@ -4,6 +4,35 @@ name="mostakim"
 age=23
 price=34.44
 print(age,price,name)
-a=None
-print(type(a))
 print(type(price))
+
+print("for comment we use # for one line comment")
+#what's app? hehe
+print("i have already commented a line")
+
+#divide will give me floating value unlike cpp or c;
+#arithmetic operators are same as cpp
+#extra operator : power operator--> a**b == a^b
+
+#relational operator is same as cpp
+
+#assignment operator is same as cpp,,, except: a**=b means 
+#a will be equal to current value of a**b;
+
+#logical operators
+# not(!) , or(||) , and(&&)
+# expr1 and expr2  ==== expr1 && expr2
+
+# type conversion: 1.type conversion-auto 
+#                  2.type casting-explicit
+
+a=3
+b=3.244
+sum=a+b  # sum becomes float by implicit conversion
+
+c="2"
+d=2.34
+# sum2=c+d  ,, this will create error
+sum2=int(c)+d
+print("sum: ",sum)
+print("sum2: ",sum2)
