2f8dfa76bd7466453e90baa3362cc496fc12cb9b
MdMostakim103 <12mdmostakim12@gmail.com>
2026-05-21 15:26:03 +0600

    Added first python file

---
+++ firstPy.py
@@ -0,0 +1,9 @@
+print("hi i am learning python","what is happeneing. ",23+45)
+print("i am what i am")
+name="mostakim"
+age=23
+price=34.44
+print(age,price,name)
+a=None
+print(type(a))
+print(type(price))
