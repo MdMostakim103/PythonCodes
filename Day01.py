2758ff64d9528a91a4ec8c21046f152575aa87e9
MdMostakim103 <12mdmostakim12@gmail.com>
2026-05-23 22:31:45 +0600

    late update

---
+++ Day01.py
@@ -0,0 +1,24 @@
+# taking input from keyboard:
+# syntax:
+#        input("here i can show msg: ") --program stops here for input taking
+# input always taken as string
+# name=input("Enter your name: ")
+# print("Welcome: ",name," your name type: ",type(name))
+# val=input("Enter a integer :")
+# print("You Entered: ",val,"but the type: ",type(val))
+
+# # for ingeter or float or any specefic type we can typecast the input function like bellow
+
+# val2=float(input("Now eneter a integer: "))
+# print ("you have entered: ",val2,"Type: ",type(val2))
+
+# question 1: take two numbers and print the sum
+
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+print("Sum of the numbers =",num1+num2)
+
+
+
+# question 2: take two numbers and print true is first is greater than or equal to b else false
+print(num1>=num2)
